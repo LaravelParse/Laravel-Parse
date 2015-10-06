bfffc5089285c95019c1ac73e8db0f69ae1c81f6
Graham Campbell <graham@alt-three.com>
2015-10-06 16:52:01 +0100

    Added extra check before registering publish command

--- src/ParseServiceProvider.php
+++ src/ParseServiceProvider.php
@@ -45,7 +45,7 @@ class ParseServiceProvider extends ServiceProvider
     {
         $source = realpath(__DIR__.'/../config/parse.php');
 
-        if (class_exists('Illuminate\Foundation\Application', false)) {
+        if (class_exists('Illuminate\Foundation\Application', false) && $app->runningInConsole()) {
             $this->publishes([$source => config_path('parse.php')]);
         } elseif (class_exists('Laravel\Lumen\Application', false)) {
             $app->configure('parse');
