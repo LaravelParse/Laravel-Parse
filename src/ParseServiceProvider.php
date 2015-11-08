28b9f862bf95898c4f445abeaee841e3d42c0314
Graham Campbell <graham@alt-three.com>
2015-11-08 19:58:44 +0000

    Improved environment specific code

--- src/ParseServiceProvider.php
+++ src/ParseServiceProvider.php
@@ -12,7 +12,9 @@
 namespace GrahamCampbell\Parse;
 
 use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Foundation\Application as LaravelApplication;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Lumen\Application as LumenApplication;
 use Parse\ParseClient;
 
 /**
@@ -45,9 +47,9 @@ class ParseServiceProvider extends ServiceProvider
     {
         $source = realpath(__DIR__.'/../config/parse.php');
 
-        if (class_exists('Illuminate\Foundation\Application', false) && $app->runningInConsole()) {
+        if ($app instanceof LaravelApplication && $app->runningInConsole()) {
             $this->publishes([$source => config_path('parse.php')]);
-        } elseif (class_exists('Laravel\Lumen\Application', false)) {
+        } elseif ($app instanceof LumenApplication) {
             $app->configure('parse');
         }
 
